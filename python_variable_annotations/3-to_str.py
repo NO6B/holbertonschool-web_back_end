9490b84da98f72c4333ada32b3f7debdd95ef017
Abdel <azrhar@MacBook-Air-de-Abdel.local>
2025-11-20 10:44:47 +0100

    fix pycode

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-"""type-annotated function to_str that takes a float n as argument
-and returns the string representation of the float."""
+"""
+type-annotated function to_str that takes a float n as argument
+and returns the string representation of the float.
+"""
 
 
-def to_str(n :float) -> str:
+def to_str(n: float) -> str:
     """function to str"""
     return str(n)
