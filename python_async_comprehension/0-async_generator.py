8c8ff1b0d8fc8aec391ae4b9c269b610b6da1503
root <root@DESKTOP-PLCA8BP.localdomain>
2025-11-29 00:09:45 +0100

    add task0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,1 +1,10 @@
+#!/usr/bin/env python3
+"""Async generator module that yields random numbers"""
+import asyncio
+import random
 
+
+async def async_generator():
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
