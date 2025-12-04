d71bcdb7242ff399931364244dc0f7252f056525
Abdel <azrhar@MacBook-Air-de-Abdel.local>
2025-12-04 09:48:55 +0100

    fix statement

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -7,4 +7,5 @@ function that lists all documents in a collection
 def list_all(mongo_collection):
     if mongo_collection is None:
         return []
-    return list(mongo_collection.find())
+    else:
+        return list(mongo_collection.find())
