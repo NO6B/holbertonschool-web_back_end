3034055bfea42a1fea77fe03df7c281781914868
Abdel <azrhar@MacBook-Air-de-Abdel.local>
2025-12-04 09:56:51 +0100

    add chmod

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -5,6 +5,7 @@ Python function that changes all topics of a school document based on the name
 
 
 def update_topics(mongo_collection, name, topics):
+    """update topics"""
     mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -5,4 +5,5 @@ Python function that returns the list of school having a specific topic
 
 
 def schools_by_topic(mongo_collection, topic):
+    """school by topics"""
     return list(mongo_collection.find({"topics": topic}))
