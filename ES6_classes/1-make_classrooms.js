9357c290d28ec1fc2c87df782e0355d123c29a84
NO6B <abdel_azrhar@hotmail.com>
2025-12-12 11:43:38 +0100

    Update 1-make_classrooms.js

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
 import ClassRoom from 0-classroom.js
 
 
-  export default function initializeRooms(params) {
+  export default function initializeRooms() {
     return [
     new ClassRoom(19),
     new ClassRoom(20),
