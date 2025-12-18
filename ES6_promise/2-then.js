fcfcaac40b433fd5cdfb1a7f4fa526817bbbffaf
Abdel <azrhar@MacBook-Air-de-Abdel.local>
2025-12-18 14:55:27 +0100

    add then and catch

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,12 @@
-export default function handleResponseFromAPI(promise){
-return new Promise((successCallback, failureCallback) => {
-    if (promise){
-        console.log("Got a response from the API")
-        successCallback({
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      return {
         status: 200,
         body: 'success'
-        });
-    }else{
-        console.log("Got a response from the API")
-        failureCallback(newError("Error"))
-    }
-});
+      };
+    })
+    .catch(() => {
+      console.error(error.message)
+    })
 }
