710044df9275dd38d4d9784e44dea3464110de1b
Abdel <azrhar@MacBook-Air-de-Abdel.local>
2026-01-07 13:53:51 +0100

    a

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,6 @@
+data = console.log("Welcome to Holberton School, what is your name?")
+process.stdin.on('data', data => {
+  console.log(`Your name is: ${data.toString()}`);
+  console.log("This important software is now closing")
+  process.exit();
+});
