50787db4e22bc9ac5235c23f1677805bb7ec419c
Abdel <azrhar@MacBook-Air-de-Abdel.local>
2026-01-07 14:07:25 +0100

    a

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,10 @@
-data = console.log("Welcome to Holberton School, what is your name?")
-process.stdin.on('data', data => {
-  console.log(`Your name is: ${data.toString()}`);
-  console.log("This important software is now closing")
-  process.exit();
+console.log("Welcome to Holberton School, what is your name?");
+
+process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+
+    console.log(`Your name is: ${name}`);
+    
+    console.log("This important software is now closing");
+    process.exit();
 });
