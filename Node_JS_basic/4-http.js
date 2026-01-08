eb999876171e47daa041edb2fc9d3af28713168a
Abdel <azrhar@MacBook-Air-de-Abdel.local>
2026-01-08 14:06:12 +0100

    a

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const app = http.createServer((request, response) => { 
+    response.writeHead(200, { 'Content-Type': 'text/plain' }); 
+    response.end('Hello Holberton School!'); 
+});
+const PORT = 1245;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
