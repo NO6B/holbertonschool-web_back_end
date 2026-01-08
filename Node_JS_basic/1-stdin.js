e279fd69f91c8e6dfa0d0311f4d57c1f5de498ee
NO6B <abdel_azrhar@hotmail.com>
2026-01-08 23:03:01 +0100

    Fix formatting and improve name output

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,13 @@
 console.log('Welcome to Holberton School, what is your name?');
 
+
+
+process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
     const name = data.toString().trim();
     if (name !== null){
-       console.log(`Your name is: ${name}`); 
-    }    
+        console.log(`Your name is: ${name}`);
+    }
 });
 
 process.stdin.on('end', () => {
