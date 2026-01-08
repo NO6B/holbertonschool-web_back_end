eedc0250ee305f0e18f0d5653f157578f00a4084
Abdel <azrhar@MacBook-Air-de-Abdel.local>
2026-01-08 15:26:09 +0100

    a

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const app = http.createServer((request, response) => { 
+    response.writeHead(200, { 'Content-Type': 'text/plain' }); 
+    
+    if(response.url === '/'){
+        console.log('Hello Holberton School!')
+    }
+    if(response.url === '/students'){
+        console.log('This is the list of our students')
+        countStudents('database.csv')
+    }
+
+});
+app.listen(1245);
+module.exports = app;
