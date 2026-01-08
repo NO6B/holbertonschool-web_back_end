21e2b2fa30ccdf010c541fe17cbe58aa2a8572c3
NO6B <abdel_azrhar@hotmail.com>
2026-01-08 23:10:52 +0100

    Fix encoding setting for stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,5 @@
 process.stdout.write('Welcome to Holberton School, what is your name?');
-
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
