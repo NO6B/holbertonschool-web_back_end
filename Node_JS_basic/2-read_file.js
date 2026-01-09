1108e4f0c92411b0c5037178161ae397b1dadc57
NO6B <abdel_azrhar@hotmail.com>
2026-01-09 11:46:39 +0100

    Refactor countStudents function for clarity

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 
-function countStudents(path) {
+const countStudents = (path) => {
   try {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.split('\n').filter((line) => line.trim() !== '');
+    
     const students = lines.slice(1);
 
     console.log(`Number of students: ${students.length}`);
@@ -11,26 +12,21 @@ function countStudents(path) {
     const fields = {};
 
     for (const line of students) {
-      const student = line.split(',');
-      const firstname = student[0];
-      const field = student[3];
+      const [firstname, , , field] = line.split(',');
 
-      if (!fields[field]) {
-        fields[field] = [];
+      if (field) {
+        if (!fields[field]) fields[field] = [];
+        fields[field].push(firstname);
       }
-      fields[field].push(firstname);
     }
 
-    for (const field in fields) {
-      if (fields.hasOwnProperty(field)) {
-        const list = fields[field];
-        console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
-      }
+    for (const [field, list] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
     }
 
   } catch (error) {
     throw new Error('Cannot load the database');
   }
-}
+};
 
 module.exports = countStudents;
