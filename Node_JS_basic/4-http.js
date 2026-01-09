e1763ceba43e81d66ba1f861b3aa9ff198233e9f
NO6B <abdel_azrhar@hotmail.com>
2026-01-09 11:21:28 +0100

    Fix response header setting in HTTP server

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -2,10 +2,9 @@ const http = require('http');
 
 const app = http.createServer((request, response) => {
     res.statusCode = 200;
-    response.writeHead('Content-Type': 'text/plain');
+    response.setHeader('Content-Type': 'text/plain');
     response.end('Hello Holberton School!');
 });
 
 app.listen(1245);
-
 module.exports = app;
